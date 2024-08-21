7d41231c85b42eb98704de36740c0b3821b8ed71
Sibgha Tullah <sibghazahid007@gmailcom>
2024-08-21 16:38:08 +0500

    Header Complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+// Import All Relevant CSS
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
+// Import All Relevant JS
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+// Import All Relevant Components
+import Header from './components/Header';
+
+
+// Import All Relevant Pages
+import Home from './pages/Home'
+import About from './pages/About'
+import Shop from './pages/Shop'
+import Contact from './pages/Contact'
+import Cart from './pages/Cart'
+import Profile from './pages/Profile'
+import NoPage from './pages/NoPage';
+
+// Import All Packages
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Header />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="shop" element={<Shop />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/pages/About.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+        About
+        About
+        About
+        About
+        About
+        About
+        About
+        About
+        About
+        About
+        About
+        About
+        About
+        About
+        About
+
+    </div>
+  )
+}
+
+export default About
